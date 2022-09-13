8a76c02d7e7a0a6df0949ff310ce2a92ce00972e
Wilmer Pocaterra <malgein17@gmail.com>
2022-09-13 10:47:02 -0400

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EmojiPickerInput from './components/emoji/emojiPickerInput';
+
+function App() {
+  return (
+    <div className="App">
+      <EmojiPickerInput />
+    </div>
+  );
+}
+
+export default App;
